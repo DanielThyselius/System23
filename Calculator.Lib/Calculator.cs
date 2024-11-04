465bb21c9efc1d3dc3d84c7167e01d664a2c14b8
Daniel Thyselius <daniel@mindfulstack.se>
2024-11-04 12:07:54 +0100

    initial

---
+++ Calculator.Lib/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.Lib;
+
+public class Calculator
+{
+    public string Name { get; set; } = "Bob";
+
+
+    public float Add(float a, float b)
+    {
+        return a + b;
+    }
+}
---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculator.Tests;
+using Calculator.Lib;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void CanAddTwoPlusThree()
+    {
+        // Arrange
+        var calc = new Calculator();
+        var expected = 5;
+        
+        // Act
+        var actual = calc.Add(2, 3);
+
+        // Assert
+        // Assert.Throws<Exception>(()=> calc.Add(0, expected));
+        Assert.Equal(expected, actual);
+        Assert.Equal("Bob", calc.Name);
+    }
+}
