5d58286690d2eecf866c6e62d49653c92943dc76
Daniel Thyselius <daniel@mindfulstack.se>
2024-11-21 10:52:45 +0100

    Add Todo project starting point

---
+++ Todo.Api/Program.cs
@@ -0,0 +1,26 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// We could do this manually, but let's use the built-in routing
+// app.MapGet("/health", () => "Healthy");
+
+app.MapHealthChecks("/health");
+
+app.Run();
---
+++ Todo.Test/HelthCheckTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Todo.Api;
+using Xunit.Sdk;
+
+namespace Todo.Test;
+
+public class HelthCheckTests : IClassFixture<WebApplicationFactory<ApiMarker>>
+{
+    private readonly HttpClient _httpClient;
+    
+    public HelthCheckTests(WebApplicationFactory<ApiMarker> factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+    
+    [Fact]
+    public async Task HealthCheckReturnsHealthy()
+    {
+        // Arrange
+        // ...
+        // Act
+        var response = await _httpClient.GetAsync("/health");
+        // Assert       
+        
+        // These two lines do essentially the same thing
+        response.EnsureSuccessStatusCode();
+        // Assert.True(response.IsSuccessStatusCode);
+    }
+}
