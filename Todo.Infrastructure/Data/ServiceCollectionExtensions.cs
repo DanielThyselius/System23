f7bd2756a60cd3bb9ca48c2d7ace046cd0d24dc0
Daniel Thyselius <daniel@mindfulstack.se>
2024-11-22 11:14:23 +0100

    with code

---
+++ Todo.Infrastructure/Data/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Todo.Infrastructure.Data;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddTodoDbContext(this IServiceCollection services)
+    {
+        services.AddDbContext<TodoDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("TodoDb");
+        });
+    }
+}
---
+++ Todo.Infrastructure/Data/TodoDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Core.Models;
+
+namespace Todo.Infrastructure.Data;
+
+public class TodoDbContext : DbContext
+{
+    internal DbSet<TodoItem> Todos { get; set; }
+
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Probably not needed... but just in case
+        modelBuilder.Entity<TodoItem>().HasKey(x => x.Id);
+
+        SeedDatabase(modelBuilder);
+    }
+
+    private static void SeedDatabase(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>().HasData(
+            new TodoItem
+            {
+                Id = 1,
+                Title = "Tvätta bilen",
+                Description = "Bilen måste tvättas, vi har en rabattkod på TvättaDinBil i stan.",
+                IsComplete = false,
+                Assignee = "Daniel"
+            },
+            new TodoItem
+            {
+                Id = 2,
+                Title = "Köpa mjölk",
+                Description = "Test",
+                IsComplete = false,
+                Assignee = "Daniel"
+            },
+            new TodoItem
+            {
+                Id = 3,
+                Title = "Köpa mjölk",
+                Description = "Test",
+                IsComplete = false,
+                Assignee = "Daniel"
+            });
+    }
+}
