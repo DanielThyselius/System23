0b06b4eb37e5e647f59ddb8ca8912eeb191de8a6
Daniel Thyselius <daniel@mindfulstack.se>
2024-11-25 10:57:44 +0100

    first test updated

--- BadProgram.Test/UnitTest1.cs
+++ BadProgram.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
 
 namespace BadProgram.Test;
 
@@ -11,9 +12,11 @@ public class UnitTest1
         var item = "Sko";
         var quantity = 3;
         var total = 27;
-        var db = new TestDatabase();
-        var datetimeProvider = new TestDateTimeProvider();
-        var sut = new ReceiptGenerator(item, quantity, db, datetimeProvider);
+        var mockDb = new Mock<IDatabase>();
+        mockDb.Setup(x => x.GetItemPrice(It.IsAny<string>())).Returns(10);
+        var mockDateTimeProvider = new Mock<IDateTimeProvider>();
+        mockDateTimeProvider.Setup(x => x.Now).Returns(DateTime.Parse("2024-01-05T12:00:00"));
+        var sut = new ReceiptGenerator(item, quantity, mockDb.Object, mockDateTimeProvider.Object);
 
         var expected = """
                         ********************************
@@ -39,25 +42,26 @@ public class UnitTest1
         // Arrange
         var item = "Sko";
         var quantity = 3;
+        //TODO: use Moq!
         var db = new TestDatabase();
         var datetimeProvider = new TestDateTimeProvider();
         var sut = new ReceiptGenerator(item, quantity, db, datetimeProvider);
-
+    
         var expected = 27;
         // Act
         var actual = sut.GetTotal();
-
+    
         // Assert
         Assert.Equal(expected, actual);
     }
 }
-
-class TestDatabase : IDatabase
-{
-    public float GetItemPrice(string id) => 10;
-}
-
-class TestDateTimeProvider : IDateTimeProvider
-{
-    public DateTime Now => DateTime.Parse("2024-01-05T12:00:00");
-}
+//
+// class TestDatabase : IDatabase
+// {
+//     public float GetItemPrice(string id) => 10;
+// }
+//
+// class TestDateTimeProvider : IDateTimeProvider
+// {
+//     public DateTime Now => DateTime.Parse("2024-01-05T12:00:00");
+// }
